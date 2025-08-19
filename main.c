10c1f6d7bea2a42cc59d486e1d6161b72da41617
mohamednayef <mohamednayef2002@gmail.com>
2025-08-20 00:35:17 +0300

    a

---
+++ main.c
@@ -0,0 +1,14 @@
+/**
+ * @author Mohamed Nayef
+ * @date Wed 20 Aug 2025 @21
+ */
+
+// ########## Include Library Section ##########
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Mohamed Nayef!\n");
+
+    return 0;
+}
