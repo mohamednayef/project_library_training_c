8de477586a69b5c94ee8fbf2bab4542a61feb499
mohamednayef <mohamednayef2002@gmail.com>
2025-08-20 00:24:21 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/**
+ * @author Mohamed Nayef
+ * @date Wed 20 Aug 2025 @21
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    cout << "Mohamed Nayef!\n";
+
+    return 0;
+}
